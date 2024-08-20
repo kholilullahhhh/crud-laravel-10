48245470e973e83438a790f9dd565efc2b9fa65f
kholilullahhhh <muhammadkholil045@gmail.com>
2024-08-21 02:26:48 +0700

    push

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,32 @@
+<!-- <div class="nk-sidebar">
+    <div class="nk-nav-scroll">
+        <ul class="metismenu" id="menu">
+            <li class="nav-label">Dashboard</li>
+            <li>
+                <a href="{{ route('dashboard') }}" aria-expanded="false">
+                    <i class="icon-speedometer menu-icon"></i><span class="nav-text">Dashboard</span>
+                </a>
+            </li>
+            <li class="nav-label">Apps</li>
+            <li>
+                <a href="{{ route('index.pelanggan') }}" aria-expanded="false">
+                    <i class="icon-envelope menu-icon"></i> <span class="nav-text">Tamu</span>
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('index.villa') }}" aria-expanded="false">
+                    <i class="icon-badge menu-icon"></i><span class="nav-text">Villa</span>
+                </a>
+            </li>
+            <li>
+                <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                    <i class="icon-screen-tablet menu-icon"></i><span class="nav-text">Transaksi</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="{{ route('create.reservasi') }}">Pemesanan</a></li>
+                    <li><a href="{{ route('index.reservasi') }}">Laporan Pemesanan</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</div> -->
